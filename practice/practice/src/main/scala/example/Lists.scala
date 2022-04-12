c960f7f7bcf79f831f39a94b2c9f432fa74c924a
FreddieMercy <junhao.zhang.freddie@gmail.com>
2022-04-12 13:17:00 -0700

    main done

--- practice/practice/src/main/scala/example/Lists.scala
+++ practice/practice/src/main/scala/example/Lists.scala
@@ -16,13 +16,32 @@ object Lists:
    *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
    *    list `xs` without its `head` element
    *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
+   * ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
+   * solution.
    *
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-  def sum(xs: List[Int]): Int = ???
+  def sum(xs: List[Int]): Int = {
+    //    if(xs == null || xs.length == 0){
+    //      throw new Exception()
+    //    }
+    sum_helper(xs, 0, xs.length);
+  }
+
+
+  def sum_helper(xs: => List[Int], start: => Int, end: => Int): Int = {
+    if (start >= end) {
+      0;
+    }
+    else if (start == end + 1) {
+      xs(start);
+    }
+    else {
+      var mid: Int = (end - start) / 2 + start;
+      sum_helper(xs, start, mid) + sum_helper(xs, mid, end);
+    }
+  }
 
   /**
    * This method returns the largest element in a list of integers. If the
@@ -37,4 +56,27 @@ object Lists:
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-  def max(xs: List[Int]): Int = ???
+  def max(xs: List[Int]): Int = {
+    max_helper(xs, 0, xs.length);
+  }
+
+  def max_helper(xs: => List[Int], start: => Int, end: => Int): Int = {
+    if (start >= end) {
+      throw new java.util.NoSuchElementException("Empty List xs");
+    }
+    else if (start == end + 1) {
+      xs(start);
+    }
+    else {
+      var mid: Int = (end - start) / 2 + start;
+      var first: Int = max_helper(xs, start, mid);
+      var second: Int = max_helper(xs, mid, end);
+
+      if (first > second) {
+        first;
+      }
+      else {
+        second;
+      }
+    }
+  }
